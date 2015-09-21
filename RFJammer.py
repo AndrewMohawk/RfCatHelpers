d28f7912b387c6dd7a4389321d0e877ad7ade299
Andrew MacPherson <andrew@andrewmohawk.com>
2015-09-22 01:53:24 +0200

    Update RFJammer.py

--- RFJammer.py
+++ RFJammer.py
@@ -19,22 +19,20 @@ def ConfigureD(d):
 	d.setFreq(frequency)
 	d.makePktFLEN(0)
 	d.setMdmDRate(4800)
-	d.setRFRegister(PA_TABLE0, 0xFF)
-	d.setRFRegister(PA_TABLE1, 0xFF)
 
 def jamFreq(d,freq):
  d.setModeTX()
 
 def stopJam(d):
  d.setModeIDLE()
- d.setRFRegister(PA_TABLE0, 0x00)
- d.setRFRegister(PA_TABLE1, 0x00) 
+
  
  
 parser = argparse.ArgumentParser(description='Application to use a RFCat compatible device to Jam a particular frequency',version="0.1-bricktop")
 parser.add_argument('-f', action="store", default="433880000", dest="baseFreq",help='Target frequency to listen for remote (default 433880000)',type=int)
 parser.add_argument('-r', action="store", dest="baudRate",default=4800,help='Baudrate, defaults to 4800',type=int)
 parser.add_argument('-t', action="store", dest="jamTime",default=15,help='Seconds to jam for, defaults to 15 seconds',type=int)
+parser.add_argument('-p', action="store", default="100", dest="power",help='Power level for re-transmitting',type=int)
 results = parser.parse_args()
 
 print "Configuring RfCat"
@@ -45,6 +43,7 @@ d.setMdmSyncMode(0)
 d.setMdmDRate(results.baudRate)
 d.setMdmChanSpc(24000)
 d.setModeIDLE()
+d.setPower(results.power)
 
 
 
